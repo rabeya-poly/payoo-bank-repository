5711d521232faec58d4a1baee8e9fed26db8a11a
rabeya poly <personhappy119@gmail.com>
2024-09-18 20:28:33 +0600

    added phone and pin

--- JS/login.js
+++ JS/login.js
@@ -4,9 +4,21 @@ document.getElementById('button-login')
 .addEventListener('click', function(event){
  // step-2: prevent default behavior   
  event.preventDefault();//<--- problem face
- console.log('login button click');
+//  console.log('login button click');
 
  //step-3: get the phone number
 const mobileNumber = document.getElementById('mobile-number').value;
-console.log(mobileNumber);
+const pinNumber = document.getElementById('pin-number').value;
+
+console.log(mobileNumber,pinNumber);
+
+//step=4: validation pin and number
+// this not do use....
+if(mobileNumber === '5' && pinNumber === '1234'){
+    console.log('you r login');
+}
+else{
+    alert('wrong number and pin')
+}
+
 })
